d71c89aab0d33ebf38c0a47604792e67c16052af
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-08-21 10:12:06 +0300

    Add license information with REUSE Specification 3.2 compatibility
    It would need to be done sooner or later with my intent to eventually publish this.

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
+# SPDX-FileCopyrightText: 2024 Aminda Suomalainen <suomalainen@aminda.eu>
+#
+# SPDX-License-Identifier: CC0-1.0
+
 all:
 	latexmk -xelatex -bibtex wifi-security.tex
--- compile.bash
+++ compile.bash
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2024 Aminda Suomalainen <suomalainen@aminda.eu>
+#
+# SPDX-License-Identifier: CC0-1.0
+
 #xelatex wifi-security.tex
 #biber wifi-security.bcf
 #xelatex wifi-security.tex
